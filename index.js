68ecde5e5804514db25b85d5f05ba860e05a689e
bethwilson07 <beth.ny.wlsn@gmail.com>
2018-11-27 15:38:01 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 //Your code here
+function justInvoke(fn) {
+  return fn();
+}
+
+function setThisWithCall(fn, thisValue, arg) {
+  return fn.call(thisValue, arg);
+}
+
+function setThisWithApply(fn, thisValue, args) {
+  const arguments = Array.prototype.slice.call(args)
+  return fn.apply(thisValue, arguments);
+}
+
+function returnNewFunctionOf(functionToBeCopied, thisValue) {
+  const newFunction = functionToBeCopied.bind(thisValue);
+  return newFunction;
+}
