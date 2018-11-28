1163143a26fe6cab2decfe4b00054f7e3a830f18
Nathan Daniel Gezahegne <nathandaniel@Nathans-MacBook-Pro.local>
2018-11-28 12:02:12 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 //Your code here
+function justInvoke(fn) {
+  return fn()
+}
+
+function setThisWithCall(fn,setThis,arg) {
+  return fn.call(setThis, arg);
+}
+
+function setThisWithApply(fn, setThis, arr) {
+  return fn.apply(setThis, arr);
+}
+
+function returnNewFunctionOf(functionToBeCopied, thisValue){
+let newFunction = functionToBeCopied.bind(thisValue)
+return newFunction;
+}
